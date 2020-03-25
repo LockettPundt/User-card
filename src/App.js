8deb11246412ab495b569b66a968ae45b9210140
Lockett Pundt <kalifornium@hotmail.com>
2020-03-25 19:51:20 -0400

    added additional instances of the UserCard component in app.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,28 @@ import React from 'react';
 import './App.css';
 import UserProfile from './components/UserProfile';
 
+const fetchUsers = async () => {
+    const response = await fetch('https://randomuser.me/api/?results=1');
+    const json = await response.json();
+    return json.results;
+}
+
+
+
 function App() {
+  const userInfo = fetchUsers();
   return (
     <div className="App">
       <UserProfile />
+      <UserProfile />
+      <UserProfile />
+      <UserProfile />
+      <UserProfile />
+      <UserProfile />
+      <UserProfile />
+      <UserProfile />
+      
+      
     </div>
   );
 }
