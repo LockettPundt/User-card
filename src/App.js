3e5fe8086db6ec6fde78df9a280b031296463b33
Lockett Pundt <kalifornium@hotmail.com>
2020-03-25 21:36:39 -0400

    moved the fetch function into the app.js and mapped over the results passing each one into an instance the info into a prop.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import './App.css';
 import UserProfile from './components/UserProfile';
 
-const fetchUsers = async () => {
-    const response = await fetch('https://randomuser.me/api/?results=1');
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userData: [],
+    }
+  }
+  fetchUsers = async (num) => {
+    const response = await fetch(`https://randomuser.me/api/?results=${num}`);
     const json = await response.json();
     return json.results;
 }
 
 
+handleClick = async () => {
+  const newUserData = await this.fetchUsers(10);
+  this.setState({
+    userData: newUserData
+  });
+};
 
-function App() {
-  const userInfo = fetchUsers();
+async componentDidMount() {
+  const userData = await this.fetchUsers(10);
+  this.setState({
+    userData: userData,
+  })
+}
+  render() {
+    const {userData} = this.state;
+    console.log("this is the userData",userData);
   return (
     <div className="App">
-      <UserProfile />
-      <UserProfile />
-      <UserProfile />
-      <UserProfile />
-      <UserProfile />
-      <UserProfile />
-      <UserProfile />
-      <UserProfile />
+      {userData.map(item => {
+        return <UserProfile rando={item} />
+      })}
+      
       
       
     </div>
   );
+  }
 }
 
 export default App;
